ad74f9b08b06cee8e1eaecfeb87b0c69679cdc0d
Ryan Nevils <ryan.nevils21@gmail.com>
2020-05-01 14:02:01 -0700

    added tests for comment cloud

---
+++ tests/testthat/test-comment_cloud.R
@@ -0,0 +1,34 @@
+test_that("valid_api_call works with valid key and video", {
+  correct_result <- 200
+
+  api_key <- "AIzaSyA1sCzxpFiBw6SG16MsWPE872_W4XfMRlE"
+  video_id <- "QjA5faZF1A8"
+
+  my_result <- valid_api_call(api_key, video_id)
+
+  expect_equal(my_result, correct_result)
+})
+
+
+test_that("valid_api_call works with bad key and video", {
+  correct_result <- 400
+
+  bad_api_key <- "AIzaSyA1sCzxpFidfdfsdfBw6SG16MsWPE872_W4XfMRlE"
+  video_id <- "QjA5faZF1A8"
+
+  my_result <- valid_api_call(bad_api_key, video_id)
+
+  expect_equal(my_result, correct_result)
+})
+
+
+test_that("valid_api_call works with valid key but bad video", {
+  correct_result <- 404
+
+  api_key <- "AIzaSyA1sCzxpFiBw6SG16MsWPE872_W4XfMRlE"
+  bad_video_id <- "QjA5fadfdsfdZF1A8"
+
+  my_result <- valid_api_call(api_key, bad_video_id)
+
+  expect_equal(my_result, correct_result)
+})
